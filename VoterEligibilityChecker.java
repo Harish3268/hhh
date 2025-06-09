4a7ecf13e966b2e7e7e332ec5ab4bc3554b8fe17
Harish3268 <harishpadatare100@gmail.com>
2025-06-09 20:04:56 +0530

    first

---
+++ VoterEligibilityChecker.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class VoterEligibilityChecker {
+
+    public static boolean isEligible(int age) {
+        return age >= 18;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (isEligible(age)) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+
+        scanner.close();
+    }
+}
